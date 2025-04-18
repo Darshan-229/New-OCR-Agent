342f1ea20aa712b2209ae8f80c4cca8a99dc3d8d
Darshan Italiya <darshanitaliya32@gmail.com>
2025-04-18 10:22:16 +0530

    add port

--- main.py
+++ main.py
@@ -71,4 +71,4 @@ def extract_text():
     return jsonify({'error': 'Unsupported file type'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
