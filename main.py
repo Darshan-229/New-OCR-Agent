10030a30b7f60759aa786506d1c0d15e761f0656
Darshan Italiya <darshanitaliya32@gmail.com>
2025-04-18 10:43:19 +0530

    change port

--- main.py
+++ main.py
@@ -70,5 +70,7 @@ def extract_text():
     print("[ERROR] Unsupported file type")
     return jsonify({'error': 'Unsupported file type'}), 400
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))  # Render will set this env variable
+    app.run(debug=True, host='0.0.0.0', port=port)
